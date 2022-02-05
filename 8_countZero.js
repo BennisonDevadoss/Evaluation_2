169cdc76c0d2f1e8afbed1c1eb042d73adf41666
Bennison Devadoss <bennisondevadoss@gmail.com>
2022-02-05 15:17:03 +0530

    Second Evaluation

---
+++ 8_countZero.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let start = 1;
+let end = 100;
+let count = 0;
+let last_number, i, j;
+for (i = start; i <= end; i++) {
+    // console.log(i);
+    j = i;
+    let length = j.toString().length;
+    // console.log(length);
+
+    for (let k = 1; k < length; k++) {
+        last_number = j % 10;
+        if (last_number == 0) {
+            count += 1;
+        }
+
+        j = j / 10;
+        j = last_number;
+    }
+
+}
+
+
+console.log(count); 
